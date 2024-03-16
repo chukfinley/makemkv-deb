1ec9b39bc369a04ca886d148c3f663b6ebeaa98e
hackerman <77645077+chukfinley@users.noreply.github.com>
2024-03-16 22:57:51 +0000

    Update checkinstall.sh

--- checkinstall.sh
+++ checkinstall.sh
@@ -3,18 +3,18 @@ sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-d
 mkdir makemkv
 cd makemkv
 echo "Downloading Makemkv"
-wget https://www.makemkv.com/download/makemkv-bin-1.17.5.tar.gz
-wget https://www.makemkv.com/download/makemkv-oss-1.17.5.tar.gz
-tar xvf makemkv-bin-1.17.5.tar.gz
-tar xvf makemkv-oss-1.17.5.tar.gz
+wget https://www.makemkv.com/download/makemkv-bin-1.17.6.tar.gz
+wget https://www.makemkv.com/download/makemkv-oss-1.17.6.tar.gz
+tar xvf makemkv-bin-1.17.6.tar.gz
+tar xvf makemkv-oss-1.17.6.tar.gz
 echo "Installing makemkv-oss"
-cd makemkv-oss-1.17.5
+cd makemkv-oss-1.17.6
 sed -i "s/av_mallocz_array/av_calloc/g" libffabi/src/ffabi.c
 ./configure
 make
 sudo checkinstall
 cd ..
-cd makemkv-bin-1.17.5
+cd makemkv-bin-1.17.6
 echo "Installing makemkv-bin"
 make
 sudo checkinstall
